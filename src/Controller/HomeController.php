34bcf74207fed2df5e01610cf04c4c18d9fcfc47
Zuilhof <b.zuilhof@plein.nl>
2018-07-23 21:18:06 +0200

    initial commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/")
+     * @Template(template="base.html.twig")
+     */
+    public function homepage()
+    {
+        return [];
+    }
+}
